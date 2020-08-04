c9ce9de6a6b4494eebf44ce9d965a5a7947b21d8
KRStoddard <kimberlynrstoddard@gmail.com>
2020-08-04 10:51:22 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    #get and set name in two methods below
+    def name
+        @name
+    end
+
+    def name=(name)
+        @name = name 
+    end
+    #get and set breed in two methods below
+    def breed
+        @breed
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+end #end of class
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+    #get and set name in two methods below
+    def name
+        @name
+    end
+
+    def name=(name)
+        @name = name
+    end
+    #get and set job in two methods below
+    def job
+        @job
+    end
+
+    def job=(job)
+        @job = job
+    end
+end #of Person class
